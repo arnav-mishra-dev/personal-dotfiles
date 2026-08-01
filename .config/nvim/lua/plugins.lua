bbf46a306a1a030ea1e57c33a1cd52f6c4a79005
Arnav Mishra <44arnavmishra@gmail.com>
2026-08-01 15:10:25 +0530

    Status line, git integration, icons and autoclosing brackets

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -28,19 +28,30 @@ vim.api.nvim_create_autocmd("UIEnter", {
 vim.pack.add{
   { src = 'https://github.com/sphamba/smear-cursor.nvim' },
   { src = 'https://github.com/mfussenegger/nvim-jdtls' },
-  { src = 'https://github.com/nvim-mini/mini.pick' },
   { src = 'https://github.com/nvim-lua/plenary.nvim' },
   { src = 'https://github.com/mikavilpas/yazi.nvim' },
   { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
   { src = 'https://github.com/nyoom-engineering/oxocarbon.nvim' },
+  { src = 'https://github.com/nvim-mini/mini.icons' },
+  { src = 'https://github.com/nvim-mini/mini.pick' },
+  { src = 'https://github.com/nvim-mini/mini.pairs' },
+  { src = 'https://github.com/nvim-mini/mini.diff' },
+  { src = 'https://github.com/nvim-mini/mini-git' },
+  { src = 'https://github.com/nvim-mini/mini.statusline' },
 }
 
+require('nvim-treesitter').install { 'lua', 'c', 'java', 'python' }
+
+require('mini.icons').setup()
 require('mini.pick').setup()
+require('mini.pairs').setup()
+require('mini.diff').setup()
+require('mini.git').setup()
+require('mini.statusline').setup()
+
 MiniPick.registry.files_fd = function()
   local command = { 'fd', '--type=f', '--no-follow', '--color=never', '--hidden' }
   return MiniPick.builtin.cli({ command = command })
 end
 
 vim.cmd("colorscheme oxocarbon")
-
-require('nvim-treesitter').install { 'lua', 'c', 'java', 'python' }
