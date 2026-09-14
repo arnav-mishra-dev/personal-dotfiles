e10940b28e3759743f0d52c2af21ff4145726704
Arnav Mishra <44arnavmishra@gmail.com>
2026-09-14 19:13:53 +0530

    Removed go setup

--- .config/nvim/lua/lsps.lua
+++ .config/nvim/lua/lsps.lua
@@ -4,7 +4,6 @@ vim.lsp.enable('rust_analyzer');
 vim.lsp.enable('ruff')
 vim.lsp.enable('basedpyright')
 vim.lsp.enable('jdtls')
-vim.lsp.enable('gopls')
 vim.lsp.enable('markdown_oxide')
 
-require('nvim-treesitter').install { 'lua', 'c', 'cpp', 'rust', 'python', 'java', 'go', 'markdown', 'markdown_inline' }
+require('nvim-treesitter').install { 'lua', 'c', 'cpp', 'rust', 'python', 'java', 'markdown', 'markdown_inline' }
