e4885f54fd3b1a82c42833a0d662ce4ff0103e84
Arnav Mishra <44arnavmishra@gmail.com>
2026-09-21 20:24:16 +0530

    Fixed yazi not launching interactively

--- .config/hypr/hyprconfig/variables.lua
+++ .config/hypr/hyprconfig/variables.lua
@@ -6,7 +6,7 @@ local terminal = "ghostty"
 -- Set programs that you use
 local vars = {
   terminal    = terminal,
-  fileManager = terminal .. " -e yazi",
+  fileManager = terminal .. " -e bash -ic yazi",
   menu        = "fuzzel",
   browser     = "brave"
 }
